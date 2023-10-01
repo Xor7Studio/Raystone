0ae83d0164b01c4d833ea4cf769c549f111aaf04
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-10-01 08:54:19 +0800

    Feat(api): improve auto reconnect

--- raystone-api/src/main/kotlin/cn/xor7/raystone/channel/EventChannelClient.kt
+++ raystone-api/src/main/kotlin/cn/xor7/raystone/channel/EventChannelClient.kt
@@ -14,33 +14,30 @@ import kotlin.math.pow
 
 
 object EventChannelClient {
-    private var eventLoopGroup: EventLoopGroup = NioEventLoopGroup()
     private var retryAttempts = 0
+    private lateinit var eventLoopGroup: EventLoopGroup
     private lateinit var bootstrap: Bootstrap
     lateinit var channel: Channel
         private set
 
-    init {
-        initBootstrap()
+    fun connect(host: String, port: Int, delay: Long = 0) {
+        eventLoopGroup = NioEventLoopGroup()
+        eventLoopGroup.schedule({ connect0(host, port) }, delay, TimeUnit.SECONDS)
     }
 
-    fun initBootstrap() {
-        bootstrap = Bootstrap()
-        bootstrap
-            .group(eventLoopGroup)
-            .channel(NioSocketChannel::class.java)
-            .option(ChannelOption.TCP_NODELAY, true)
-            .option(ChannelOption.SO_KEEPALIVE, true)
-            .handler(object : ChannelInitializer<SocketChannel>() {
-                override fun initChannel(ch: SocketChannel) {
-                    ch.pipeline().addLast(LengthFieldBasedFrameDecoder(Int.MAX_VALUE, 1, 4))
-                    ch.pipeline().addLast(EventChannelHandler)
-                }
-            })
-    }
-
-    fun connect(host: String, port: Int) {
+    private fun connect0(host: String, port: Int) {
         try {
+            bootstrap = Bootstrap()
+                .group(eventLoopGroup)
+                .channel(NioSocketChannel::class.java)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(object : ChannelInitializer<SocketChannel>() {
+                    override fun initChannel(ch: SocketChannel) {
+                        ch.pipeline().addLast(LengthFieldBasedFrameDecoder(Int.MAX_VALUE, 1, 4))
+                        ch.pipeline().addLast(EventChannelHandler)
+                    }
+                })
             bootstrap.connect(host, port).addListener(GenericFutureListener { future: ChannelFuture ->
                 if (future.isSuccess) {
                     channel = future.channel()
@@ -50,10 +47,7 @@ object EventChannelClient {
                     if (retryAttempts <= Raystone.apiConfig.maxRetryAttempts) {
                         val delaySeconds = 2.0.pow(retryAttempts.toDouble()).toLong()
                         println("Connection attempt $retryAttempts failed. Retrying in $delaySeconds seconds...")
-                        eventLoopGroup.shutdownGracefully()
-                        eventLoopGroup = NioEventLoopGroup()
-                        initBootstrap()
-                        eventLoopGroup.schedule({ connect(host, port) }, delaySeconds, TimeUnit.SECONDS)
+                        connect(host, port, delaySeconds)
                     } else {
                         println("Max retry attempts reached. Connection failed.")
                     }
